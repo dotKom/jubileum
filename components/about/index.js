08ac95422adcc419956882b937268d837fb6096b
Nils Herde <mail@hernil.com>
2016-09-19 14:47:26 +0200

    Loads of minor css fixes. Needs cleaning!

--- components/about/index.js
+++ components/about/index.js
@@ -3,9 +3,9 @@ const About = () => {
     <header id="about" className="component">
 
       <h1>ONLINE 30 ÅR!</h1>
-      <p className="about-description">Linjeforeningen Online ble grunnlagt i 1985 på AVG på Lade og mer lorem ipsum. Onlines første leder var Ole Breivik, en real kjernekar som ingen som studerer nå vet hver er. Håper han kommer på jubileet, og håper han liker steinbit. Det har ikke noe med saken å gjøre, men likevel. Det er fett med jubileum, og vi har arrangementer hele uken! </p>
+      <p className="about-description">Linjeforeningen Online ble grunnlagt høsten 1985 på Den allmennvitenskapelige høgskole på Lade. Tre tiår senere skal vi feire opprettelsen, og alle årene imellom. Jubileumskomiteen inviterer alle nåværende og tidligere Online-medlemmer til en ukelang feiring i Trondheim. Lørdag 12. november setter vi kronen på verket med jubileumsgalla. Velkommen!</p>
 
-      <a href="#order" className="orderTicket">BESTILL BILLETT</a>
+      <a href="https://online.ntnu.no/events/331/onlines-30-arsjubileum-bankett/" className="orderTicket">BESTILL BILLETT</a>
 
       <img className="about-header-logo" src="assets/images/online_logo.svg" alt="" />
     </header>
