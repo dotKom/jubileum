f0e57d3bad6b092a51b7a1ed3908364170ffa715
Nils Herde <mail@hernil.com>
2016-09-19 11:22:27 +0200

    Removed unused and deleted modules

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ import D from 'react-dom';
 require('./styles/base.scss');
 
 import About from './components/about';
-import Fadder from './components/fadder';
-import Partners from './components/partners';
 import Calendar from './components/calendar';
-import Join from './components/join';
-import Contact from './components/contact';
-import Slack from './components/slack';
-import Social from './components/social';
 
 const App = () => {
   return (
@@ -18,13 +12,6 @@ const App = () => {
       <div className="orange-border">
         <div className="white-border">
           <About />
-          {/*<Fadder />
-           <Calendar />
-           <Join />
-           <Slack />
-           <Social />
-           <Contact />
-           <Partners />*/}
         </div>
       </div>
     </div>
